b680440a1fd768020cb731e26f5498ac401568e3
TK <aethertk57@gmail.com>
2026-09-25 22:56:56 +0530

    Expose OS-backed PIN lock

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -2,6 +2,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('ddklab', Object.freeze({
   platform: process.platform,
   version: '0.1.0',
+  security: {
+    getPinState: () => ipcRenderer.invoke('security:getPinState'),
+    setPin: (pin) => ipcRenderer.invoke('security:setPin', pin),
+    verifyPin: (pin) => ipcRenderer.invoke('security:verifyPin', pin)
+  },
   storage: {
     saveSession: (session) => ipcRenderer.invoke('storage:saveSession', session),
     listSessions: () => ipcRenderer.invoke('storage:listSessions'),
