87ed72c188449c88515ed091e0a118f55afdada9
TK <aethertk57@gmail.com>
2026-09-25 22:56:07 +0530

    Add local encrypted session IPC bridge

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,5 +1,15 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('ddklab', Object.freeze({
   platform: process.platform,
-  version: '0.1.0'
+  version: '0.1.0',
+  security: {
+    getPinState: () => ipcRenderer.invoke('security:getPinState'),
+    setPin: (pin) => ipcRenderer.invoke('security:setPin', pin),
+    verifyPin: (pin) => ipcRenderer.invoke('security:verifyPin', pin)
+  },
+  storage: {
+    saveSession: (session) => ipcRenderer.invoke('storage:saveSession', session),
+    listSessions: () => ipcRenderer.invoke('storage:listSessions'),
+    loadSession: (id) => ipcRenderer.invoke('storage:loadSession', id)
+  }
 }));
