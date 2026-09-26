4bdc1c837197a3c4ba3377ad89eb7564a463083d
TK <aethertk57@gmail.com>
2026-09-26 10:41:29 +0530

    Bump DDKLab service-worker cache for DSP v17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ddklab-v15';
+const CACHE='ddklab-v17';
 const ASSETS=['./','./index.html','./dsp-v5.js','./ddklab-enhancements.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
